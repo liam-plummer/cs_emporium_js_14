6a162e3562aae1ed43f95d6eda5998ab0193b151
Liam <liam.f.plummer@gmail.com>
2020-10-02 08:34:23 +0200

    commit 5

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,18 @@ btn[2].addEventListener("click", () => {
     body.style.backgroundColor = "black"
     nav.style.backgroundColor = "black"
 })
+
+console.log(btn);
+// Carousel
+// let carImage = document.querySelectorAll(".carousel_pic")
+// let carousel = document.getElementById("images")
+// btn[9].addEventListener("click", () => {
+//     carImage[0].style.opacity = "0"
+//     carImage[0].style.transition = "opacity 1s"
+
+//     setTimeout(() => {
+//         carImage[0].remove()
+//         carImage[1].style.opacity = "1"
+//         carImage[1].style.transition = "opacity 1s"
+//     }, 1000);
+// })
